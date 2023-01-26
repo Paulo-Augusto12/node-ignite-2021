38b18387f056c6ec7215942e637aae0b2679dd49
Paulo Augusto <paulo-augusto12@outlook.com>
2023-01-26 15:01:05 -0300

    feat: fim da exemplificação

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
 const express = require("express");
+const crypto = require("node:crypto");
+
+const app = express();
+const id = crypto.randomUUID();
+
+const courses = []
+
+app.get("/courses", (req, res) => {
+  return res.json([
+    {
+      id,
+      curso: "curso 1",
+      tema: "tema",
+    },
+  ]);
+});
+
+app.post("/courses", (req, res) => {
+//   const { curso, tema } = req.body;
+  return res.json({
+    id,
+    curso:"Um curso muito legal",
+    tema: "Um tema de curso muito legal",
+  });
+});
+
+app.put("/courses/:id", (req, res) => {
+  return res.json(["curso 3"]);
+});
+
+app.patch("/courses/:id", (req, res) => {});
+
+app.delete("/courses/:id", (req, res) => {
+  return res.json();
+});
+
+app.listen(3333);
